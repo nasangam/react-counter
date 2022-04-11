f0116c4abc2750219942a5a160d5f188921e099d
Nagaraju Sangam <nagaraju.sangam@Nagarajus-MBP.Dlink>
2022-04-12 01:02:14 +0530

    husky

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useState } from 'react';
 function App() {
   const [counter, setCounter] = useState(0);
   return (
-    <div className="App" style={{margin:"50px"}}>
+    <div className="App" style={{margin:"50px", fontSize: "20px"}}>
       <div>counter {counter} </div>
       <div><button onClick={()=>setCounter(counter+1)}>Incriment</button></div>
     </div>
