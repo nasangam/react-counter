dde307f190e9537eb1267fab9672c5dbed6070b0
nasangam <nagaraju_sangam@yahoo.co.in>
2022-04-12 01:39:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     const [counter, setCounter] = useState(0);
   return (
     <div className="App" style={{margin:"50px", fontSize: "20px"}}>
-        <div>counter {counter} </div>
+        <div>counter ::: {counter} </div>
       <div><button onClick={()=>setCounter(counter+1)}>Incriment</button></div>
     </div>
   );
