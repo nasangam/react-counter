0c15d31f308559673fec4b2089d4e798c21f590d
Nagaraju Sangam <nagaraju.sangam@Nagarajus-MBP.Dlink>
2022-04-12 09:17:51 +0530

    env

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
     const [counter, setCounter] = useState(0);
   return (
     <div className="App" style={{margin:"50px", fontSize: "20px"}}>
-       <div> :::Counter Application:::</div><hr/>
+       <div>===::Counter Application::===</div><hr/>
         <div>counter : {counter} </div>
       <div><button onClick={()=>setCounter(counter+1)}>Incriment</button></div>
+      <div>API_KEY: {process.env.REACT_APP_MY_API_KEY}</div>
+      <div>API_KEY: {process.env.NODE_ENV}</div>
     </div>
   );
 }
